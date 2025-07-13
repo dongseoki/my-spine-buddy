9640e4a386159448768b4871e1c79fc0f3c417af
Dong Seok Lee <dongseok.lee.log@gmail.com>
2025-07-13 13:17:23 +0900

    [feat] layout에 moduls.css적용~

--- src/app/(with-search-group)/layout.tsx
+++ src/app/(with-search-group)/layout.tsx
@@ -1,13 +1,9 @@
-import { ReactNode } from "react";
+import styles from "./layout.module.css";
 
-export default function Layout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <div>임시 서치바</div>
+    <div className={styles.container}>
+      <div className={styles.title}>임시 서치바</div>
       {children}
     </div>
   );
