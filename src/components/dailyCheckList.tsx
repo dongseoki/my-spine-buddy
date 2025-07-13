253c7fa589f60f4f7b8ae7c492988a15224fe6a0
Dong Seok Lee <dongseok.lee.log@gmail.com>
2025-07-13 14:01:20 +0900

    [feat] controlled component 추가

--- src/components/dailyCheckList.tsx
+++ src/components/dailyCheckList.tsx
@@ -1,32 +1,35 @@
 "use client";
 
+import { useState } from "react";
+
+const checkListMockData = [
+  { id: 1, name: "걸을 때 바른 자세 체크?", checked: false },
+  { id: 2, name: "앉아 있을 때 바른 자세 체크?", checked: false },
+  { id: 3, name: "취침시 바른 자세 체크?", checked: false },
+];
+
 export default function DailyCheckList() {
   console.log("DailyCheckList");
+  const [checkList, setCheckList] = useState(checkListMockData);
+  function handleCheck(id: number) {
+    console.log("id : ",id, "Checked : ");
+    const newCheckList = checkList.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setCheckList(newCheckList);
+    console.log("newCheckList : ",newCheckList);
+  }
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full bg-white rounded-lg shadow-md border border-gray-300 border-collapse">
         <tbody>
-          <tr>
-            <td className="px-4 py-2 border border-gray-300 flex items-center">
-              <input type="checkbox" id="walk" className="mr-2" />
-              <label htmlFor="walk">걸을 때 바른 자세 체크?</label>
-            </td>
-            <td className="px-4 py-2 border border-gray-300"></td>
-          </tr>
-          <tr>
-            <td className="px-4 py-2 border border-gray-300 flex items-center">
-              <input type="checkbox" id="sit" className="mr-2" />
-              <label htmlFor="sit">앉아 있을 때 바른 자세 체크?</label>
-            </td>
-            <td className="px-4 py-2 border border-gray-300"></td>
-          </tr>
-          <tr>
-            <td className="px-4 py-2 border border-gray-300 flex items-center">
-              <input type="checkbox" id="sleep" className="mr-2" />
-              <label htmlFor="sleep">취침시 바른 자세 체크?</label>
-            </td>
-            <td className="px-4 py-2 border border-gray-300"></td>
-          </tr>
+          {checkList.map((item) => (
+            <tr key={item.id}>
+              <td className="px-4 py-2 border border-gray-300 flex items-center">
+                <input type="checkbox" id={item.name} className="mr-2" checked={item.checked} onChange={() => handleCheck(item.id)} />
+                <label htmlFor={item.name}>{item.name}</label>
+              </td>
+              <td className="px-4 py-2 border border-gray-300"></td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
