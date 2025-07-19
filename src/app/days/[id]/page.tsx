402fdf64e4a35027efb17421c5b0ee2a8ce4b048
Dong Seok Lee <dongseok.lee.log@gmail.com>
2025-07-19 15:34:31 +0900

    [fix]페이지 파라미터를 Promise로 변경하고, 해당 값을 사용하여 API 호출 및 UI 업데이트 수정

--- src/app/days/[id]/page.tsx
+++ src/app/days/[id]/page.tsx
@@ -1,16 +1,18 @@
 import { DayDetailResponseBody } from "@/app/api/day-detail/[dayValue]/route";
 
 interface PageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
 import DailyCheckList from "../../../components/dailyCheckList";
 
 export default async function DayPage({ params }: PageProps) {
+  const { id } = await params;
+  
   const dayDetailResponse = await fetch(
-    `http://localhost:3000/api/day-detail/${params.id}`
+    `http://localhost:3000/api/day-detail/${id}`
   );
   if (!dayDetailResponse.ok) {
     console.error("Failed to fetch day detail");
@@ -22,12 +24,12 @@ export default async function DayPage({ params }: PageProps) {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold text-center mb-8">
-        허리일지 {params.id} 페이지
+        허리일지 {id} 페이지
       </h1>
       <h2>오늘의 내 감정 {dayDetailResponseBody.feeling}</h2>
       <div className="bg-white rounded-lg shadow-md p-6 mb-8">
         <p className="text-gray-600 text-center">
-          여기에 허리일지 {params.id}의 상세 내용이 표시됩니다.
+          여기에 허리일지 {id}의 상세 내용이 표시됩니다.
         </p>
       </div>
       <DailyCheckList initialCheckList={dayDetailResponseBody.checkList} />
